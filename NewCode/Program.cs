c6316f341a6b1a5c33ee1600a162035260c61853
Matthew Luchini <luch@Matthews-MBP.lan>
2023-01-27 11:25:16 -0500

    NewCode

---
+++ NewCode/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Welcome to the Jungle");
+Console.WriteLine("Cocktails start out at 15$");
+Console.WriteLine("If you need anything please don't heistate to reach out");
+Console.WriteLine("Once created, the cocktail Budda has been our best seller");
+Console.WriteLine("Please enjoy!");
+
+
+
